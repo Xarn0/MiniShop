a3614fb87a3ec91ebce3defc7b71d10d09dd1847
xarn0 <xouli@list.ru>
2025-06-22 17:48:07 +0300

    fix: config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-	publicPath: `/MiniShop/`,
+	base: `/MiniShop/`,
 	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {
